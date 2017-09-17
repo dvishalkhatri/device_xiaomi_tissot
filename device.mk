a04db017647ad6be540e1d5a24f1a25d1563be2f
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    tissot: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -101,3 +101,11 @@ PRODUCT_PACKAGES_DEBUG += \
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
+
+# Camera
+PRODUCT_PACKAGES += \
+    Gcam
+
+#USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.xiaomi_8996
